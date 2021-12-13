8255e994ea920da5c07f5e9a3d4e604a2d5df81d
Josivaldo João <josivaldojoao14@gmail.com>
2021-12-12 23:53:06 -0300

    mudança no gitignore

---
+++ dist/precache-manifest.6864a1ac0edf842c8908b4b9169a9859.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24451388c2fc69419fac47ca08311257",
+    "url": "/_redirects"
+  },
+  {
+    "revision": "305d83dd52418c8eaa9c",
+    "url": "/css/app.b4293ada.css"
+  },
+  {
+    "revision": "7e6fccf52ab383b152478319f9550a96",
+    "url": "/img/Logo.7e6fccf5.png"
+  },
+  {
+    "revision": "cf6ab76628e45906285e696a9cd03f74",
+    "url": "/img/emptycart.cf6ab766.png"
+  },
+  {
+    "revision": "df4d8f2f0853d6d38778e6f025d9235d",
+    "url": "/img/home.df4d8f2f.png"
+  },
+  {
+    "revision": "b3756064c9b3a9d68f68eccb81aae097",
+    "url": "/img/icon-branco.b3756064.png"
+  },
+  {
+    "revision": "0634694ac5697511b37e8fd8da4efcbe",
+    "url": "/img/logoPink.0634694a.png"
+  },
+  {
+    "revision": "80cb9532f847cddc5fd02b9fe442ec31",
+    "url": "/img/sorry.80cb9532.jpg"
+  },
+  {
+    "revision": "84a7829aba6f10f001d5ab9dc888eb25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "305d83dd52418c8eaa9c",
+    "url": "/js/app.b9b5b739.js"
+  },
+  {
+    "revision": "dcda118da906bf4474c5",
+    "url": "/js/chunk-vendors.c3510874.js"
+  },
+  {
+    "revision": "8f3b884b51a30654422562558b256fee",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
